2572227f8ed2658e1377b9e53749de0aa9dfd4b2
Alex Tulic <alex.tulic@gmail.com>
2018-09-24 13:00:57 -0700

    add basic persistence

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { persistor, store } from './store';
 
 // blueprintjs css
-import "../node_modules/normalize.css/normalize.css";
-import "../node_modules/@blueprintjs/core/lib/css/blueprint.css";
-import "../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css";
+import '../node_modules/normalize.css/normalize.css';
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css';
+import '../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
-  <Provider store={createStore(rootReducer, devToolsEnhancer())}>
-    <App />
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 registerServiceWorker();
